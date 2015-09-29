423d59184ecd6d2575d92889173d1a091a9f9c73
Josh McKelvey <jmckelvey0@gmail.com>
2015-09-29 14:52:42 -0400

    initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'profiles/search'
 
-  get 'profiles/profile'
+  post 'profiles/profile'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
